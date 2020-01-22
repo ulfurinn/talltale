9ec6eaa9e19b83b8e2b13670a55ab33ceefe6e52
Valeri Sokolov <ulfurinn@ulfurinn.net>
2020-01-22 14:28:58 +0100

    removed ra-admin

--- src/Editor.js
+++ src/Editor.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-import { Admin } from "react-admin";
-import jsonServerProvider from "ra-data-json-server";
 
-const dataProvider = jsonServerProvider("/editor");
+
+
 
 class Editor extends Component {
     render() {
-        return (<Admin dataProvider={dataProvider} />);
+        return <div/>;
     }
 }
 export default Editor;
