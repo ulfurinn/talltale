317eecdb07fdd132775a6136d6a45bd685885307
Valeri Sokolov <ulfurinn@ulfurinn.net>
2023-09-09 22:29:35 +0200

    more efficient card draw
    fewer list scans per drawn card (1 instead of 3);
    fewer allocations

--- lib/talltale/game/deck.ex
+++ lib/talltale/game/deck.ex
@@ -12,54 +12,29 @@ defmodule Talltale.Game.Deck do
     has_many :cards, Card
   end
 
-  def draw(%__MODULE__{cards: cards}, n) do
-    draw(cards, n)
-  end
-
   def draw(cards, n) when is_list(cards) do
-    draw(cards, n, [])
+    max_endpoint = Enum.reduce(cards, 0, &(&1.frequency + &2))
+    draw(cards, min(length(cards), n), max_endpoint, [])
   end
 
-  defp draw(_, 0, acc), do: acc
-  defp draw([], _, acc), do: acc
+  defp draw(cards, n, max_endpoint, acc)
+  defp draw(_, 0, _, acc), do: acc
 
-  defp draw(cards, n, acc) do
-    {cards, card} = draw(cards)
-    draw(cards, n - 1, [card | acc])
+  defp draw(cards, n, max_endpoint, acc) do
+    point = Enum.random(0..(max_endpoint - 1))
+
+    {cards, card} = take_at_point(cards, 0, point)
+    draw(cards, n - 1, max_endpoint - card.frequency, [card | acc])
   end
 
-  defp draw(cards) do
-    {cards, index} = build_ranges(cards)
+  defp take_at_point([card | tail], acc, point) do
+    card_point = acc + card.frequency
 
-    cards = Enum.reverse(cards)
-    index = Enum.random(0..(index - 1))
-    {cards, {_, card}} = take_when(cards, fn {range, _} -> index in range end)
-    {cards, card}
-  end
-
-  defp build_ranges(cards) do
-    cards
-    |> Enum.reduce({[], 0}, fn card, {cards, index} ->
-      card =
-        case card do
-          {_, card} -> card
-          card -> card
-        end
-
-      range = index..(index + card.frequency - 1)
-      {[{range, card} | cards], index + card.frequency}
-    end)
-  end
-
-  defp take_when(list, predicate)
-  defp take_when([], _), do: {[], nil}
-
-  defp take_when([el | tail], predicate) do
-    if predicate.(el) do
-      {tail, el}
+    if point >= acc && point < card_point do
+      {tail, card}
     else
-      {tail, result} = take_when(tail, predicate)
-      {[el | tail], result}
+      {tail, drawn_card} = take_at_point(tail, card_point, point)
+      {[card | tail], drawn_card}
     end
   end
 end
