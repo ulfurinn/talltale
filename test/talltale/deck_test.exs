3e242ce4cc8023377a4b5bd9970f57b05a6f2059
Valeri Sokolov <ulfurinn@ulfurinn.net>
2023-09-09 22:15:36 +0200

    fix test

--- test/talltale/deck_test.exs
+++ test/talltale/deck_test.exs
@@ -1,16 +1,16 @@
 defmodule Talltale.DeckTest do
   use ExUnit.Case
 
-  alias Talltale.Card
-  alias Talltale.Deck
+  alias Talltale.Game.Card
+  alias Talltale.Game.Deck
 
   test "gets some cards" do
     cards = [
-      %Card{frequency: 1, action: 1},
-      %Card{frequency: 1, action: 2},
-      %Card{frequency: 1, action: 3},
-      %Card{frequency: 1, action: 4},
-      %Card{frequency: 1, action: 5}
+      %Card{frequency: 1, id: 1},
+      %Card{frequency: 1, id: 2},
+      %Card{frequency: 1, id: 3},
+      %Card{frequency: 1, id: 4},
+      %Card{frequency: 1, id: 5}
     ]
 
     draw = Deck.draw(cards, 3)
@@ -19,8 +19,8 @@ defmodule Talltale.DeckTest do
 
   test "gets as many cards as there are" do
     cards = [
-      %Card{frequency: 1, action: 1},
-      %Card{frequency: 1, action: 2}
+      %Card{frequency: 1, id: 1},
+      %Card{frequency: 1, id: 2}
     ]
 
     draw = Deck.draw(cards, 3)
@@ -31,15 +31,15 @@ defmodule Talltale.DeckTest do
     1..100_000
     |> Enum.each(fn _ ->
       cards = [
-        %Card{frequency: 10_000, action: 1},
-        %Card{frequency: 1, action: 2},
-        %Card{frequency: 1, action: 3},
-        %Card{frequency: 1, action: 4},
-        %Card{frequency: 1, action: 5}
+        %Card{frequency: 10_000, id: 1},
+        %Card{frequency: 1, id: 2},
+        %Card{frequency: 1, id: 3},
+        %Card{frequency: 1, id: 4},
+        %Card{frequency: 1, id: 5}
       ]
 
       draw = Deck.draw(cards, 3)
-      assert Enum.find(draw, &(&1.action == 1))
+      assert Enum.find(draw, &(&1.id == 1))
     end)
   end
 end
