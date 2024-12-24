0a91ad2b9d28fb49d4f789e441ad7ae03522e2b9
Valeri Sokolov <ulfurinn@ulfurinn.net>
2024-12-24 20:54:37 +0100

    browser logging

--- config/dev.exs
+++ config/dev.exs
@@ -51,6 +51,7 @@ config :tall_tale, TallTaleWeb.Endpoint,
 # Watch static and templates for browser reloading.
 config :tall_tale, TallTaleWeb.Endpoint,
   live_reload: [
+    web_console_logger: true,
     patterns: [
       ~r"priv/static/(?!uploads/).*(js|css|png|jpeg|jpg|gif|svg)$",
       ~r"priv/gettext/.*(po)$",
